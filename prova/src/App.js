df0bee5993cf0e0a061e1314dc76e56d03ade38d
Hildo paz <hildopaz1@gmail.com>
2021-11-03 16:18:47 -0300

    quarto

--- prova/src/App.js
+++ prova/src/App.js
@@ -4,16 +4,22 @@ import {
 import { Routes } from './routes'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/style.css'
-
+import Sidebar from "./components/template/Sidebar";
+import Header from "./components/template/Header";
+import Footer from "./components/template/Footer";
+import ToggleMenu from "./components/template/Sidebar";
 
 function App() {
   return (
-    <>
+    <div className="sidebar-menu-collapsed">
+      <Sidebar/>
+      <ToggleMenu/>
+      <Header/>
       <Router>
         <Routes/>
       </Router>
-
-    </>
+      <Footer/>
+    </div>
   );
 }
 
